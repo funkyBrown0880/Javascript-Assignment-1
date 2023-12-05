ecaeead04d04f4d5cc33cf1911cfcca67ca606d8
olufunke1011 <owoseniolufunke1011@gmail.com>
2023-12-05 09:43:12 +0100

    first question

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
  // A function that checks if the input/parameter is even
 
- function even (num) {
-    let num = prompt("Enter a number");
-    if (num % 2 === 0);console.log("Number is even");
-}else{
-    console.log("Not an even number")
-} 
+
+    function calEven (num){ 
+     if (num % 2 === 0) { return `${num} is an even number`; } 
+     else { return `${num} is not an even number`; };
+     } console.log (calEven (2));
