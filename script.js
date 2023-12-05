19ad7840df06991474b598690b887e088d65862d
olufunke1011 <owoseniolufunke1011@gmail.com>
2023-12-05 09:28:55 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ // A function that checks if the input/parameter is even
+
+ function even (num) {
+    let num = prompt("Enter a number");
+    if (num % 2 === 0);console.log("Number is even");
+}else{
+    console.log("Not an even number")
+} 
