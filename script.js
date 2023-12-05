6b9654a6c32f13c87e02aeddbd06622cb883c002
olufunke1011 <owoseniolufunke1011@gmail.com>
2023-12-05 09:47:10 +0100

    second question

--- script.js
+++ script.js
@@ -5,3 +5,12 @@
      if (num % 2 === 0) { return `${num} is an even number`; } 
      else { return `${num} is not an even number`; };
      } console.log (calEven (2));
+
+
+    //  a function that prints to the console n1 to n2. where n1 is the first input/parameter and n2 is the second input/parameter
+
+    function nameTwo (n1, n2) { 
+        for (let n = n1; n<=n2; n++) { console.log(n); } 
+    } nameTwo (5, 40);
+
+    
